bd0ebe15ec667b1919a003230194674372c774d1
daisyalexandria <dazavala7@gmail.com>
2023-09-03 13:31:21 -0500

    initial commit

---
+++ db/migrate/20230903175448_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :photos do |t|
+      t.string :name
+      t.integer :width
+      t.integer :height
+
+      t.timestamps
+    end
+  end
+end
